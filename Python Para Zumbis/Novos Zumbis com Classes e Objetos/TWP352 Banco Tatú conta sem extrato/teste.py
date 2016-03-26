e6194cfa16ba5c298b713a9637d9f4f2ac1489d2
davilaeduardo <contato@eduardodavila.com.br>
2016-03-26 18:05:07 -0300

    TWP352 Banco Tatú conta sem extrato

---
+++ Python Para Zumbis/Novos Zumbis com Classes e Objetos/TWP352 Banco Tatú conta sem extrato/teste.py
@@ -0,0 +1,7 @@
+from TWP352 import Cliente
+
+joao = Cliente('Joao da Silva', '777-1234')
+maria = Cliente('Maria da Silva', '555-4321')
+print joao.nome
+print joao.telefone
+print ('Nome: %s e Telefone: %s' % (joao.nome, joao.telefone))
