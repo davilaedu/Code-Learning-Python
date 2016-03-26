d339e5bb9d397eddac1977c568dc66b2db96e57b
davilaeduardo <contato@eduardodavila.com.br>
2016-03-26 17:12:21 -0300

    TWP352 Banco Tatú conta sem extrato

--- Python Para Zumbis/Novos Zumbis com Classes e Objetos/TWP350 Classes e Objetos/TWP350.py
+++ Python Para Zumbis/Novos Zumbis com Classes e Objetos/TWP350 Classes e Objetos/TWP350.py
@@ -2,3 +2,15 @@ class Televisao:
     def __init__(self):
         self.ligada = False
         self.canal = 2
+
+    def muda_canal_para_baixo(self):
+        self.canal -= 1
+
+    def muda_canal_para_cima(self):
+        self.canal += 1
+
+
+tv = Televisao()
+print tv.muda_canal_para_cima()
+print tv.muda_canal_para_cima()
+print tv.canal
